4e63ca0c4270a5afa83b206692636092cb796868
Artem Kurkin <artykn88@gmail.com>
2023-11-20 18:09:39 +0300

    Update donation.py

--- app/models/donation.py
+++ app/models/donation.py
@@ -4,5 +4,6 @@ from app.models.base import AbstractBase
 
 
 class Donation(AbstractBase):
+    """ Donation model. """
     user_id = Column(Integer, ForeignKey('user.id'))
     comment = Column(Text)
