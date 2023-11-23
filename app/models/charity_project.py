fe1e61b552ddb23c033c2ca784aedba1edb92a81
Artem Kurkin <artykn88@gmail.com>
2023-11-23 16:06:16 +0300

    Update charity_project.py

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -4,5 +4,6 @@ from app.models.base import AbstractBase
 
 
 class CharityProject(AbstractBase):
+    """ Project """
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=False)
